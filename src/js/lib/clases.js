948ec3c1cee2f8bf5a12b1acc68c463840a8b907
Carlos Andres Alzate <unarteman@gmail.com>
2024-06-02 19:30:42 -0300

    Agrego documentacion jsdoc

--- src/js/lib/clases.js
+++ src/js/lib/clases.js
@@ -1,9 +1,15 @@
 /**
  * @file clases.js
- *
+ * @description Archivo con definiciones de las clases para gestion de errores.
  */
 
 // gestion de mensajes de error
+/**
+ * @class InvalidCountError
+ * @extends {Error}
+ * @classdesc Error que se lanza cuando el recuento de personajes es invalido
+ * @param {string} message - El mensaje de error
+ */
 class InvalidCountError extends Error {
   constructor(message) {
     super(message);
@@ -11,6 +17,12 @@ class InvalidCountError extends Error {
   }
 }
 
+/**
+ * @class OutOfRangeError
+ * @extends {Error}
+ * @classdesc Error que se lanza cuando un valor esta fuera del rango esperado.
+ * @param {string} message - el mensaje de error.
+ */
 class OutOfRangeError extends Error {
   constructor(message) {
     super(message);
@@ -18,7 +30,14 @@ class OutOfRangeError extends Error {
   }
 }
 
-// Errores para manejo especifico de la API
+/**
+ * @class APIError
+ * @extends {Error}
+ * @classdesc Error que se lanza cuando hay un problema con la respuesta de
+ * laAPI
+ * @param {string} message - El mensaje de error
+ * @param {number} statusCode - El codigo de estado de la respuesta API
+ */
 class APIError extends Error {
   constructor(message, statusCode) {
     super(message);
@@ -27,7 +46,11 @@ class APIError extends Error {
   }
 }
 
-//  Manejo de errores de red
+/**
+ * @class NetworkError
+ * @extends {Error}
+ * @classdesc Error que se lanza cuando hay un problema de red
+ */
 class NetworkError extends Error {
   constructor() {
     super('Error de red');
